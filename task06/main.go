a3c0689db3183252c8ca3c5450f4d9b91afa7c51
saushew <saushev123@gmail.com>
2023-02-28 04:13:39 +0300

    fix task06

--- task06/main.go
+++ task06/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"sync"
 )
 
@@ -13,6 +14,13 @@ func func0() {
 
 func func1(wg *sync.WaitGroup) {
 	defer wg.Done()
+	defer fmt.Println("func2 completed")
+
+	return
+}
+
+func func2(wg *sync.WaitGroup) {
+	defer wg.Done()
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovering from panic in func1:", r)
@@ -20,13 +28,6 @@ func func1(wg *sync.WaitGroup) {
 	}()
 
 	panic("func1 completed")
-}
-
-func func2(wg *sync.WaitGroup) {
-	defer wg.Done()
-	defer fmt.Println("func2 completed")
-
-	return
 }
 
 func func3(ctx context.Context, wg *sync.WaitGroup) {
@@ -53,26 +54,42 @@ func func4(stop <-chan struct{}, wg *sync.WaitGroup) {
 	}
 }
 
+func func5(wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer fmt.Println("func4 completed")
+
+	runtime.Goexit()
+}
+
 func main() {
 	wg := &sync.WaitGroup{}
 
+	// завершение с основным потоком
 	go func0()
 
+	// заверешение по выполнению работы
 	wg.Add(1)
 	go func1(wg)
 
+	// паника
 	wg.Add(1)
 	go func2(wg)
 
+	// остановка контекстом
 	wg.Add(1)
 	ctx, cancel := context.WithCancel(context.Background())
 	go func3(ctx, wg)
 	cancel()
 
+	// остановка отдельным stop каналом
 	wg.Add(1)
 	stop := make(chan struct{})
 	go func4(stop, wg)
 	close(stop)
+
+	// остановка через runtime.Goexit()
+	wg.Add(1)
+	go func5(wg)
 
 	wg.Wait()
 }
